71640dc0db96db88038bc48416403be2d8a85bb0
Bluzzi <camilledugas.pro@gmail.com>
2025-04-24 12:59:39 +0200

    setup shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
 import { eslintConfig } from "@bluzzi/eslint-config";
+import { globalIgnores } from "eslint/config";
 
-export default eslintConfig({
-  typescript: { tsconfigPath: "./tsconfig.json" },
-});
+export default eslintConfig(
+  {
+    typescript: { tsconfigPath: "./tsconfig.json" },
+  },
+  {
+    ignores: globalIgnores(["ui/**/*"], "Ignore UI directory"),
+  },
+);
